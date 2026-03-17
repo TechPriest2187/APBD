0beeb6028e526b2ccbf6eb634f4588b16310837e
TechPriest2187 <nazkarukr2007@gmail.com>
2026-03-17 21:41:47 +0100

    task 6 2

--- class2/task6.cs
+++ class2/task6.cs
@@ -1,1 +1,9 @@
-int calculateMin(int[] values);
+int calculateMin(int[] values)
+{
+    int min = values[0];
+    for (int i = 1; i < values.Length; i++)
+    {
+        min = Math.Min(min, values[i]);
+    }
+    return min;
+}
